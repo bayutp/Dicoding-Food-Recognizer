6cf228221da249329b99d48f0052e92897fb9a87
bayu <bayuteguhpamuji@gmail.com>
2025-09-24 15:14:39 +0700

    [bayi] - create fitur image classifications

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -2,6 +2,9 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:food_recognizer_app/controller/home_provider.dart';
+import 'package:food_recognizer_app/controller/image_classification_provider.dart';
+import 'package:food_recognizer_app/service/image_classification_service.dart';
+import 'package:food_recognizer_app/static/classifications_state.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,8 +12,16 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => HomeProvider(),
+    return MultiProvider(
+      providers: [
+        Provider(create: (context) => ImageClassificationService()),
+        ChangeNotifierProvider(
+          create: (context) => ImageClassificationProvider(
+            context.read<ImageClassificationService>(),
+          ),
+        ),
+        ChangeNotifierProvider(create: (context) => HomeProvider()),
+      ],
       child: const _HomeView(),
     );
   }
@@ -41,8 +52,28 @@ class _HomeView extends StatelessWidget {
   }
 }
 
-class _HomeBody extends StatelessWidget {
+class _HomeBody extends StatefulWidget {
   const _HomeBody();
+
+  @override
+  State<_HomeBody> createState() => _HomeBodyState();
+}
+
+class _HomeBodyState extends State<_HomeBody> {
+  late ImageClassificationProvider provider;
+  String? imagePath;
+
+  @override
+  void dispose() {
+    provider.close();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    provider = context.read<ImageClassificationProvider>();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,7 +86,7 @@ class _HomeBody extends StatelessWidget {
               Expanded(
                 child: Consumer<HomeProvider>(
                   builder: (context, value, child) {
-                    final imagePath = value.imagePath;
+                    imagePath = value.imagePath;
                     return imagePath == null
                         ? Align(
                             alignment: Alignment.center,
@@ -69,6 +100,46 @@ class _HomeBody extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 8),
+              Consumer<ImageClassificationProvider>(
+                builder: (context, value, child) {
+                  if (value.classification.isEmpty) {
+                    return SizedBox.shrink();
+                  }
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          value.classification.entries.first.key,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 12,
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisSize:
+                              MainAxisSize.min, // biar ukuran pas isi konten
+                          children: const [
+                            Text('Food detail'),
+                            SizedBox(width: 8), // jarak antara teks dan icon
+                            Icon(Icons.arrow_forward), // icon di sebelah kanan
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+              SizedBox(height: 8),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
@@ -78,30 +149,86 @@ class _HomeBody extends StatelessWidget {
                     children: [
                       Expanded(
                         child: ElevatedButton(
-                          onPressed: () =>
-                              context.read<HomeProvider>().openGallery(),
+                          onPressed: () => {
+                            context.read<HomeProvider>().openGallery(),
+                            provider.reset(),
+                          },
                           child: const Text("Gallery"),
                         ),
                       ),
                       SizedBox(width: 8),
                       Expanded(
                         child: ElevatedButton(
-                          onPressed: () =>
-                              context.read<HomeProvider>().openCamera(),
+                          onPressed: () => {
+                            context.read<HomeProvider>().openCamera(),
+                            provider.reset(),
+                          },
                           child: const Text("Camera"),
                         ),
                       ),
                     ],
                   ),
                   SizedBox(height: 8),
-                  FilledButton.tonal(
-                    onPressed: () {
-                      final scaffoldMessenger = ScaffoldMessenger.of(context);
-                      scaffoldMessenger.showSnackBar(
-                        SnackBar(content: Text("Feature under development 🙏")),
-                      );
+                  Consumer<ImageClassificationProvider>(
+                    builder: (context, provider, _) {
+                      return switch (provider.state) {
+                        ClassificationsLoadingState() =>
+                          const FilledButton.tonal(
+                            onPressed: null,
+                            child: SizedBox(
+                              width: 20,
+                              height: 20,
+                              child: CircularProgressIndicator(strokeWidth: 2),
+                            ),
+                          ),
+                        ClassificationsLoadedState() => FilledButton.tonal(
+                          onPressed: () async {
+                            if (imagePath != null && imagePath!.isNotEmpty) {
+                              final bytes = await File(
+                                imagePath!,
+                              ).readAsBytes();
+                              await provider.runClassifications(bytes);
+                            }
+                          },
+                          child: const Text("Analyze"),
+                        ),
+                        ClassificationsErrorState(:final error) => Column(
+                          children: [
+                            Text(
+                              'Error: $error',
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                            FilledButton.tonal(
+                              onPressed: () async {
+                                if (imagePath != null &&
+                                    imagePath!.isNotEmpty) {
+                                  final bytes = await File(
+                                    imagePath!,
+                                  ).readAsBytes();
+                                  await provider.runClassifications(bytes);
+                                }
+                              },
+                              child: const Text("Retry"),
+                            ),
+                          ],
+                        ),
+                        _ => FilledButton.tonal(
+                          onPressed: () async {
+                            if (imagePath != null && imagePath!.isNotEmpty) {
+                              final bytes = await File(
+                                imagePath!,
+                              ).readAsBytes();
+                              await provider.runClassifications(bytes);
+                            }
+                          },
+                          child: const Text("Analyze"),
+                        ),
+                      };
                     },
-                    child: const Text("Analyze"),
                   ),
                 ],
               ),
